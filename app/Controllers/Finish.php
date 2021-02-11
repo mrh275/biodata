013983fc97dd467fcb1d220e3691ca2010e9a56a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-02-11 13:36:21 +0700

    Add finish page after submit all form

---
+++ app/Controllers/Finish.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Finish extends BaseController
+{
+    public function index()
+    {
+        return view('front/done');
+    }
+
+    //--------------------------------------------------------------------
+
+}
