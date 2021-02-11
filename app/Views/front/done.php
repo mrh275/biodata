8a8ab8d460137231ad95ccd34e6a44d399662c2d
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-02-11 13:37:29 +0700

    Add finish layout for finish controller

---
+++ app/Views/front/done.php
@@ -0,0 +1,27 @@
+<?= $this->extend('template/front-finish') ?>
+
+<?= $this->section('content') ?>
+
+<section class="container-fluid">
+    <div class="container">
+        <div class="row mt-5 mb-5">
+            <div class="col-lg-12 text-center">
+                <h3 class="mt-2 mb-4">
+                    FORMULIR BIODATA SISWA <br>
+                    SMA NEGERI 1 RAWAMERTA <br>
+                    TAHUN PELAJARAN 2020/2021
+                </h3>
+            </div>
+        </div>
+        <div class="row mt-3 mb-5">
+            <div class="col-lg-12 text-center">
+                <h2 class="finish mt-3 mb-5">
+                    TERIMA KASIH! <br>
+                    SUDAH MENGISI FORMULIR BIODATA SISWA.
+                </h2>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?= $this->endSection() ?>
