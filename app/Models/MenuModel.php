6dfc11d9d5cbe4c130f9c7bad4971a7a31f5d2b1
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-02-24 01:11:55 +0700

    Add model for get menu from database

---
+++ app/Models/MenuModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class MenuModel extends Model
+{
+    protected $table = 'biodata_siswa';
+    public function getMenu()
+    {
+        return $this->db->table('menu')->get()->getResultArray();
+    }
+
+    public function getSubMenu($menuId)
+    {
+        $querySubMenu = "SELECT *
+                FROM `submenu` JOIN `menu`
+                ON `submenu`.`menu_id` = `menu`.`id`
+                WHERE `submenu`.`menu_id` = $menuId
+                AND `submenu`.`active` = 1
+            ";
+        return $this->db->query($querySubMenu)->getResultArray();
+    }
+
+    public function getSubmenuBy($submenu)
+    {
+        return $this->db->table('submenu')->where(['slug_submenu' => $submenu])->get()->getRowArray();
+    }
+}
