ee4ef09e76400a74d55736a2f8f7a2447af03f0e
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-02-24 01:10:11 +0700

    Add login page for admin or users

---
+++ app/Controllers/Login.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Login extends BaseController
+{
+    public function index()
+    {
+        return view('auth/login');
+    }
+
+    //--------------------------------------------------------------------
+
+}
