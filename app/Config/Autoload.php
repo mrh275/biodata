e4818a76cc9d399083b1980ee31eb67dc13c06d1
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-02-24 01:08:12 +0700

    Add new library irsyadulibad/datatables

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -42,6 +42,7 @@ class Autoload extends AutoloadConfig
 	public $psr4 = [
 		APP_NAMESPACE => APPPATH, // For custom app namespace
 		'Config'      => APPPATH . 'Config',
+		'Irsyadulibad\DataTables'   => APPPATH . 'Libraries/codeigniter4-datatables/src',
 	];
 
 	/**
