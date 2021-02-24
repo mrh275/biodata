9b48b43dbd7f18212d66fcc17bc47baf99ec9aaa
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-02-24 13:56:28 +0700

    Add new data column and properties for layout column

--- app/Views/datatable/datasiswa.php
+++ app/Views/datatable/datasiswa.php
@@ -1,13 +1,16 @@
 <table id="datasiswa" class="table table-bordered table-hover">
     <thead>
-        <tr>
+        <tr class="text-center">
             <th>NO</th>
             <th>NAMA SISWA</th>
             <th>NISN</th>
             <th>NIS</th>
+            <th>NIK</th>
+            <th>JENIS KELAMIN</th>
             <th>TEMPAT LAHIR</th>
             <th>TANGGAL LAHIR</th>
             <th>KELAS</th>
+            <th>ALAMAT</th>
         </tr>
     </thead>
     <tbody>
@@ -21,9 +24,16 @@
                 <td><?= $siswa['nama'] ?></td>
                 <td><?= $siswa['nisn'] ?></td>
                 <td><?= $siswa['nis'] ?></td>
+                <td><?= $siswa['nik'] ?></td>
+                <?php if ($siswa['jenis_kelamin'] == "L") : ?>
+                    <td>Laki - laki</td>
+                <?php else : ?>
+                    <td>Perempuan</td>
+                <?php endif; ?>
                 <td><?= $siswa['tempat_lahir'] ?></td>
                 <td><?= $siswa['tanggal_lahir'] ?></td>
                 <td><?= $siswa['kelas'] ?></td>
+                <td><?= $siswa['alamat'] ?></td>
             </tr>
         <?php endforeach; ?>
     </tbody>
@@ -32,6 +42,18 @@
 <script>
     //Datatable Datasiswa
     $(document).ready(function() {
-        $("#datasiswa").DataTable();
+        $("#datasiswa").DataTable({
+            "scrollX": true,
+            "scrollY": true,
+            "columnDefs": [{
+                    width: 180,
+                    targets: 1
+                },
+                {
+                    width: 180,
+                    targets: 9
+                }
+            ],
+        });
     });
 </script>
