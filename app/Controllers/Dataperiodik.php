f2b6230cd06c0a3dedf262f42d7046d290b9c120
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-02-28 22:24:05 +0700

    Add datatables function for data periodik siswa

--- app/Controllers/Dataperiodik.php
+++ app/Controllers/Dataperiodik.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use Irsyadulibad\DataTables\DataTables;
+
 class Dataperiodik extends BaseController
 {
     public function index()
@@ -115,6 +117,131 @@ class Dataperiodik extends BaseController
         $this->dataPeriodikModel->add($data);
 
         return redirect()->to('/dataupload');
+    }
+
+    public function view()
+    {
+        $submenu = 'data-periodik';
+
+        $data = [
+            'menu'          => 'data-siswa',
+            'listMenu'      => $this->menuModel->getMenu(),
+            'submenuSlug'   => $submenu,
+            'submenuName'   => $this->menuModel->getSubmenuBy($submenu),
+            'title'         => 'Data Periodik Siswa | Mini Dapodik',
+        ];
+
+        return view('admin/data-periodik', $data);
+    }
+
+    public function getPeriodikSiswa()
+    {
+        if ($this->request->isAJAX()) {
+            $data = [
+                'dataperiodik'     => $this->dataPeriodikModel->getPeriodikSiswa(),
+            ];
+
+            $msg = [
+                'data'  => view('datatable/periodiksiswa', $data),
+            ];
+            echo json_encode($msg);
+        } else {
+            exit('Data tidak ditemukan. Silahkan hubungi admin.');
+        }
+    }
+
+    // public function tambahPeriodik()
+    // {
+    //     if ($this->request->isAJAX()) {
+    //         $data  = [
+    //             'validation'        => \Config\Services::validation(),
+    //             'dataperiodik'      => $this->dataPeriodikModel->getPeriodikByNis($nis),
+    //             'datasiswa'         => $this->dataPeriodikModel->getSiswaByNis($nis),
+    //             'kelas'             => $this->dataPeriodikModel->getKelas(),
+    //         ];
+
+    //         $msg = [
+    //             'data'  => view('modal/tambahsiswamodal', $data),
+    //         ];
+    //         echo json_encode($msg);
+    //     } else {
+    //         exit('Data tidak ditemukan. Silahkan hubungi admin.');
+    //     }
+    // }
+
+    public function editperiodik()
+    {
+        if ($this->request->isAJAX()) {
+            $nis = $this->request->getVar('nis');
+            $data  = [
+                'validation'        => \Config\Services::validation(),
+                'dataperiodik'      => $this->dataPeriodikModel->getPeriodikByNis($nis),
+                'datasiswa'         => $this->dataPeriodikModel->getSiswaByNis($nis),
+                'kelas'             => $this->dataPeriodikModel->getKelas(),
+            ];
+
+            $msg = [
+                'data'  => view('modal/editperiodikmodal', $data),
+            ];
+            echo json_encode($msg);
+        } else {
+            exit('Halaman tidak ditemukan. Silahkan hubungi admin.');
+        }
+    }
+
+    public function updatePeriodik()
+    {
+        if ($this->request->isAJAX()) {
+
+            //Ambil data dari form
+            $data = [
+                'nis'               => $this->request->getVar('nis'),
+                'asal_sekolah'      => $this->request->getVar('asal_sekolah'),
+                'nomor_peserta_un'  => $this->request->getVar('nomor_peserta_un'),
+                'nomor_seri_ijazah' => $this->request->getVar('nomor_seri_ijazah'),
+                'nomor_seri_skhun'  => $this->request->getVar('nomor_seri_skhun'),
+                'tinggi_badan'      => $this->request->getVar('tinggi_badan'),
+                'berat_badan'       => $this->request->getVar('berat_badan'),
+                'hobi'              => $this->request->getVar('hobi'),
+                'cita_cita'         => $this->request->getVar('cita_cita'),
+                'jarak_rumah'       => $this->request->getVar('jarak_rumah'),
+                'waktu_tempuh'      => $this->request->getVar('waktu_tempuh'),
+                'updated_at'        => date('Y-m-d')
+            ];
+
+            $nis = $this->request->getVar('nis');
+
+            //Kirim data ke model
+            $this->dataPeriodikModel->updateDataperiodik($nis, $data);
+
+            //Pesan berhasil memasukan data
+            $msg = [
+                'sukses' => "Data Siswa sudah diubah.",
+            ];
+
+
+            //Encode/convert array ke json
+            echo json_encode($msg);
+        } else {
+            //Escape error direct access
+            exit('Halaman tidak ditemukan. Silahkan hubungi admin.');
+        }
+    }
+
+    public function hapusPeriodik()
+    {
+        if ($this->request->isAJAX()) {
+            $nis = $this->request->getVar('nis');
+
+            $this->dataPeriodikModel->hapusPeriodikByNis($nis);
+
+            $msg = [
+                'sukses' => "Data Siswa telah berhasil dihapus.",
+            ];
+
+            //Encode/convert array ke json
+            echo json_encode($msg);
+        }
     }
 
     //--------------------------------------------------------------------
--- app/Models/DataperiodikModel.php
+++ app/Models/DataperiodikModel.php
@@ -7,9 +7,40 @@ use CodeIgniter\Model;
 class DataperiodikModel extends Model
 {
     protected $allowedFields = ['nis', 'asal_sekolah', 'nomor_peserta_un', 'nomor_seri_ijazah', 'nomor_seri_skhun', 'tinggi_badan', 'berat_badan', 'hobi', 'cita_cita', 'jarak_tempuh', 'waktu_tempuh', ''];
+    protected $table = 'data_periodik';
 
     public function add($data)
     {
         $this->db->table('data_periodik')->insert($data);
+    }
+
+    public function getPeriodikSiswa()
+    {
+        return $this->db->table($this->table)->select('biodata_siswa.nama as nama,data_periodik.nis as nis,biodata_siswa.kelas as kelas,data_periodik.asal_sekolah as asalsekolah,data_periodik.nomor_peserta_un as nopesun,data_periodik.nomor_seri_ijazah as noijazah, data_periodik.nomor_seri_skhun as noskhun,data_periodik.hobi as hobi, data_periodik.cita_cita as cita')->join('biodata_siswa', 'biodata_siswa.nis=data_periodik.nis')->get()->getResultArray();
+    }
+
+    public function getSiswaByNis($nis)
+    {
+        return $this->db->table('biodata_siswa')->where(['nis' => $nis])->get()->getRowArray();
+    }
+
+    public function getPeriodikByNis($nis)
+    {
+        return $this->db->table('data_periodik')->where(['nis' => $nis])->get()->getRowArray();
+    }
+
+    public function updateDataPeriodik($nis, $data)
+    {
+        $this->db->table('data_periodik')->where(['nis' => $nis])->update($data);
+    }
+
+    public function hapusPeriodikByNis($nis)
+    {
+        $this->db->table('data_periodik')->where(['nis' => $nis])->delete();
+    }
+
+    public function getKelas()
+    {
+        return $this->db->table('kelas')->get()->getResultArray();
     }
 }
