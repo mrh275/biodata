30a4efca4dc9df3210c3ceec05d9b9bb5140ff68
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-02-28 22:27:30 +0700

    Fix breadcumb link

--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -14,8 +14,8 @@
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard v1</li>
+                        <li class="breadcrumb-item"><a href="<?= base_url('/dashboard') ?>">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard</li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
