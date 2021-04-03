7352e0fbc4e3f61a9730a6655214a23a24d7ab00
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2021-04-04 03:09:04 +0700

    Port form method to ajax

--- app/Controllers/Biodata.php
+++ app/Controllers/Biodata.php
@@ -9,208 +9,206 @@ class Biodata extends BaseController
     public function index()
     {
         $data  = [
-            'validation'        => \Config\Services::validation(),
-            'jenis_kelamin'     => $this->biodataModel->getJK(),
-            'kelas'             => $this->biodataModel->getKelas(),
-            'agama'             => $this->biodataModel->getAgama(),
-            'tempat_tinggal'    => $this->biodataModel->getTinggal(),
-            'transportasi'      => $this->biodataModel->getTransportasi(),
+            'title'             => 'Form Biodata | PPDB SMA Negeri 1 Rawamerta',
         ];
 
         return view('front/home', $data);
     }
 
+    public function form()
+    {
+        if ($this->request->isAJAX()) {
+            $data = [
+                'validation'        => \Config\Services::validation(),
+                'jenis_kelamin'     => $this->biodataModel->getJK(),
+                'kelas'             => $this->biodataModel->getKelas(),
+                'agama'             => $this->biodataModel->getAgama(),
+                'tempat_tinggal'    => $this->biodataModel->getTinggal(),
+                'transportasi'      => $this->biodataModel->getTransportasi(),
+            ];
+
+            $msg = [
+                'data' => view('front/biodata', $data),
+                'isi'   => 'Hallo'
+            ];
+
+            echo json_encode($msg);
+        } else {
+            exit('Halaman tidak ditemukan!');
+        }
+    }
+
     public function add()
     {
-        $this->db      = \Config\Database::connect();
-        $dataNis = $this->request->getVar('nis');
 
-        if ($this->biodataModel->where(['nis' => $dataNis])->first() == TRUE) {
-            session()->setFlashdata('nis_ada', '<strong>NIS sudah ada!</strong> Silahkan masukan NIS lain. Jika ingin mengubah data, silahkan hubungi admin.');
-            return redirect()->to('/biodata')->withInput();
-        }
-
-        if (!$this->validate([
-            'nama'    => [
-                'rules' => 'required[nama]',
-                'errors'    => [
-                    'required'  => 'Nama Lengkap wajib diisi!'
-                ]
-            ],
-            'jk'    => [
-                'rules' => 'required[jk]',
-                'errors'    => [
-                    'required'  => 'Jenis kelamin wajib diisi!'
-                ]
-            ],
-            'kelas'    => [
-                'rules' => 'required[kelas]',
-                'errors'    => [
-                    'required'  => 'Kelas wajib diisi!'
-                ]
-            ],
-            'nis'    => [
-                'rules' => 'required[nis]|numeric|min_length[9]|max_length[9]',
-                'errors'    => [
-                    'required'  => 'Nomor Induk Sekolah (NIS) wajib diisi!',
-                    'numeric'   => 'Hanya boleh memasukan angka.',
-                    'min_length' => 'Jumlah angka boleh yang dimasukan hanya 9 karakter.',
-                    'max_length' => 'Jumlah angka boleh yang dimasukan hanya 9 karakter.'
-                ]
-            ],
-            'nisn'    => [
-                'rules' => 'required[nisn]|numeric|min_length[10]|max_length[10]',
-                'errors'    => [
-                    'required'  => 'Nomor Induk Siswa Nasional (NISN) wajib diisi!',
-                    'numeric'   => 'Hanya boleh memasukan angka.',
-                    'min_length' => 'Jumlah angka boleh yang dimasukan hanya 10 karakter.',
-                    'max_length' => 'Jumlah angka boleh yang dimasukan hanya 10 karakter.'
-                ]
-            ],
-            'nik'    => [
-                'rules' => 'required[nik]|numeric|min_length[16]|max_length[16]',
-                'errors'    => [
-                    'required'  => 'Nomor Induk Kependudukan (NIK) wajib diisi!',
-                    'numeric'   => 'Hanya boleh memasukan angka.',
-                    'min_length' => 'Jumlah angka boleh yang dimasukan hanya 16 karakter.',
-                    'max_length' => 'Jumlah angka boleh yang dimasukan hanya 16 karakter.'
-                ]
-            ],
-            'kk'    => [
-                'rules' => 'required[kk]|numeric|min_length[16]|max_length[16]',
-                'errors'    => [
-                    'required'  => 'Nomor Kartu Keluarga wajib diisi!',
-                    'numeric'   => 'Hanya boleh memasukan angka.',
-                    'min_length' => 'Jumlah angka boleh yang dimasukan hanya 16 karakter.',
-                    'max_length' => 'Jumlah angka boleh yang dimasukan hanya 16 karakter.'
-                ]
-            ],
-            'anak_ke_berapa'    => [
-                'rules' => 'required[anak_ke_berapa]|numeric|max_length[1]',
-                'errors'    => [
-                    'required'  => 'Silahkan isi terlebih dahulu anak keberapa anda!?',
-                    'numeric'   => 'Harap hanya masukan angka saja.',
-                    'max_length' => 'Cukup masukan satu digit angka saja.'
-                ]
-            ],
-            'tempatlahir'    => [
-                'rules' => 'required[tempatlahir]',
-                'errors'    => [
-                    'required'  => 'Tempat lahir wajib diisi!',
-                ]
-            ],
-            'tgl_lahir'    => [
-                'rules' => 'required[tgl_lahir]|valid_date',
-                'errors'    => [
-                    'required'  => 'Tanggal lahir wajib diisi!',
-                ]
-            ],
-            'agama'    => [
-                'rules' => 'required[agama]',
-                'errors'    => [
-                    'required'  => 'Agama wajib diisi!',
-                ]
-            ],
-            'alamat'    => [
-                'rules' => 'required[alamat]',
-                'errors'    => [
-                    'required'  => 'Alamat wajib diisi!',
-                ]
-            ],
-            'rt'    => [
-                'rules' => 'required[rt]|numeric|max_length[3]',
-                'errors'    => [
-                    'required'  => 'Nomor RT wajib diisi!',
-                    'numeric'   => 'Hanya bisa memasukan angka saja.',
-                    'max_length' => 'Maksimal angka yang dimasukan hanya 3 digit.'
-                ]
-            ],
-            'rw'    => [
-                'rules' => 'required[rw]|numeric|max_length[3]',
-                'errors'    => [
-                    'required'  => 'Nomor RW wajib diisi!',
-                    'numeric'   => 'Hanya bisa memasukan angka saja.',
-                    'max_length' => 'Maksimal angka yang dimasukan hanya 3 digit.'
-                ]
-            ],
-            'dusun'    => [
-                'rules' => 'required[dusun]',
-                'errors'    => [
-                    'required'  => 'Nama Dusun/Kampung wajib diisi!',
-                ]
-            ],
-            'desa'    => [
-                'rules' => 'required[desa]',
-                'errors'    => [
-                    'required'  => 'Desa/Kelurahan wajib diisi!',
-                ]
-            ],
-            'kecamatan'    => [
-                'rules' => 'required[kecamatan]',
-                'errors'    => [
-                    'required'  => 'Kecamatan wajib diisi!',
-                ]
-            ],
-            'pos'    => [
-                'rules' => 'required[pos]|numeric|max_length[5]',
-                'errors'    => [
-                    'required'  => 'Kode POS wajib diisi!',
-                    'numeric'   => 'Hanya bisa memasukan angka saja.',
-                    'max_length' => 'Maksimal angka yang dimasukan hanya 5 digit.'
-                ]
-            ],
-            'tinggal'    => [
-                'rules' => 'required[tinggal]',
-                'errors'    => [
-                    'required'  => 'Tempat tinggal wajib diisi!',
-                ]
-            ],
-            'transportasi'    => [
-                'rules' => 'required[transportasi]',
-                'errors'    => [
-                    'required'  => 'Mode Transportasi wajib diisi!',
-                ]
-            ],
-        ])) {
+        if ($this->request->isAJAX()) {
             $validation = \Config\Services::validation();
-            return redirect()->to('/biodata')->withInput()->with('validation', $validation);
+            if (!$this->validate([
+                'nama'    => [
+                    'rules' => 'required[nama]',
+                    'errors'    => [
+                        'required'  => 'Nama Lengkap wajib diisi!'
+                    ]
+                ],
+                'nisn'    => [
+                    'rules' => 'required[nisn]|numeric|min_length[10]|max_length[10]',
+                    'errors'    => [
+                        'required'  => 'Nomor Induk Siswa Nasional (NISN) wajib diisi!',
+                        'numeric'   => 'Hanya boleh memasukan angka.',
+                        'min_length' => 'Jumlah angka boleh yang dimasukan hanya 10 karakter.',
+                        'max_length' => 'Jumlah angka boleh yang dimasukan hanya 10 karakter.'
+                    ]
+                ],
+                'nik'    => [
+                    'rules' => 'required[nik]|numeric|min_length[16]|max_length[16]',
+                    'errors'    => [
+                        'required'  => 'Nomor Induk Kependudukan (NIK) wajib diisi!',
+                        'numeric'   => 'Hanya boleh memasukan angka.',
+                        'min_length' => 'Jumlah angka boleh yang dimasukan hanya 16 karakter.',
+                        'max_length' => 'Jumlah angka boleh yang dimasukan hanya 16 karakter.'
+                    ]
+                ],
+                'kk'    => [
+                    'rules' => 'required[kk]|numeric|min_length[16]|max_length[16]',
+                    'errors'    => [
+                        'required'  => 'Nomor Kartu Keluarga wajib diisi!',
+                        'numeric'   => 'Hanya boleh memasukan angka.',
+                        'min_length' => 'Jumlah angka boleh yang dimasukan hanya 16 karakter.',
+                        'max_length' => 'Jumlah angka boleh yang dimasukan hanya 16 karakter.'
+                    ]
+                ],
+                'anak_ke_berapa'    => [
+                    'rules' => 'required[anak_ke_berapa]|numeric|max_length[1]',
+                    'errors'    => [
+                        'required'  => 'Silahkan isi terlebih dahulu anak keberapa anda!?',
+                        'numeric'   => 'Harap hanya masukan angka saja.',
+                        'max_length' => 'Cukup masukan satu digit angka saja.'
+                    ]
+                ],
+                'tempatlahir'    => [
+                    'rules' => 'required[tempatlahir]',
+                    'errors'    => [
+                        'required'  => 'Tempat lahir wajib diisi!',
+                    ]
+                ],
+                'tgl_lahir'    => [
+                    'rules' => 'required[tgl_lahir]|valid_date',
+                    'errors'    => [
+                        'required'  => 'Tanggal lahir wajib diisi!',
+                    ]
+                ],
+                'alamat'    => [
+                    'rules' => 'required[alamat]',
+                    'errors'    => [
+                        'required'  => 'Alamat wajib diisi!',
+                    ]
+                ],
+                'rt'    => [
+                    'rules' => 'required[rt]|numeric|max_length[3]',
+                    'errors'    => [
+                        'required'  => 'Nomor RT wajib diisi!',
+                        'numeric'   => 'Hanya bisa memasukan angka saja.',
+                        'max_length' => 'Maksimal angka yang dimasukan hanya 3 digit.'
+                    ]
+                ],
+                'rw'    => [
+                    'rules' => 'required[rw]|numeric|max_length[3]',
+                    'errors'    => [
+                        'required'  => 'Nomor RW wajib diisi!',
+                        'numeric'   => 'Hanya bisa memasukan angka saja.',
+                        'max_length' => 'Maksimal angka yang dimasukan hanya 3 digit.'
+                    ]
+                ],
+                'dusun'    => [
+                    'rules' => 'required[dusun]',
+                    'errors'    => [
+                        'required'  => 'Nama Dusun/Kampung wajib diisi!',
+                    ]
+                ],
+                'desa'    => [
+                    'rules' => 'required[desa]',
+                    'errors'    => [
+                        'required'  => 'Desa/Kelurahan wajib diisi!',
+                    ]
+                ],
+                'kecamatan'    => [
+                    'rules' => 'required[kecamatan]',
+                    'errors'    => [
+                        'required'  => 'Kecamatan wajib diisi!',
+                    ]
+                ],
+                'pos'    => [
+                    'rules' => 'required[pos]|numeric|max_length[5]',
+                    'errors'    => [
+                        'required'  => 'Kode POS wajib diisi!',
+                        'numeric'   => 'Hanya bisa memasukan angka saja.',
+                        'max_length' => 'Maksimal angka yang dimasukan hanya 5 digit.'
+                    ]
+                ],
+            ])) {
+
+
+                $msg = [
+                    'error' => [
+                        'nama'          => $validation->getError('nama'),
+                        'nisn'          => $validation->getError('nisn'),
+                        'nik'           => $validation->getError('nik'),
+                        'noKK'          => $validation->getError('kk'),
+                        'anakKeBerapa'  => $validation->getError('anak_ke_berapa'),
+                        'tempatlahir'   => $validation->getError('tempatlahir'),
+                        'tgl_lahir'     => $validation->getError('tgl_lahir'),
+                        'akte'          => $validation->getError('no_akte'),
+                        'alamat'        => $validation->getError('alamat'),
+                        'rt'            => $validation->getError('rt'),
+                        'rw'            => $validation->getError('rw'),
+                        'dusun'         => $validation->getError('dusun'),
+                        'desa'          => $validation->getError('desa'),
+                        'kecamatan'     => $validation->getError('kecamatan'),
+                        'pos'           => $validation->getError('pos'),
+                    ],
+                ];
+
+                echo json_encode($msg);
+            } else {
+                $data = [
+                    'no_daftar'          => $this->request->getVar('no_regis'),
+                    'nama'              => $this->request->getVar('nama'),
+                    'jenis_kelamin'     => $this->request->getVar('jk'),
+                    'nisn'              => $this->request->getVar('nisn'),
+                    'nik'               => $this->request->getVar('nik'),
+                    'no_kk'             => $this->request->getVar('kk'),
+                    'anak_ke_berapa'    => $this->request->getVar('anak_ke_berapa'),
+                    'tempat_lahir'      => $this->request->getVar('tempatlahir'),
+                    'tanggal_lahir'     => $this->request->getVar('tgl_lahir'),
+                    'no_akte'           => $this->request->getVar('no_akte'),
+                    'agama'             => $this->request->getVar('agama'),
+                    'alamat'            => $this->request->getVar('alamat'),
+                    'rt'                => $this->request->getVar('rt'),
+                    'rw'                => $this->request->getVar('rw'),
+                    'dusun'             => $this->request->getVar('dusun'),
+                    'desa'              => $this->request->getVar('desa'),
+                    'kecamatan'         => $this->request->getVar('kecamatan'),
+                    'pos'               => $this->request->getVar('pos'),
+                    'tempat_tinggal'    => $this->request->getVar('tinggal'),
+                    'transportasi'      => $this->request->getVar('transportasi'),
+                    'created_at'        => date('Y-m-d H:i:s')
+                ];
+
+                $sesi = [
+                    'no_daftar'   => $this->request->getVar('no_regis'),
+                ];
+
+                session()->set($sesi);
+
+                $this->biodataModel->add($data);
+
+                $msg = [
+                    'sukses'    => 'Data berhasil ditambahkan'
+                ];
+
+                echo json_encode($msg);
+            }
         }
-
-        $data = [
-            'nama'              => $this->request->getVar('nama'),
-            'jenis_kelamin'     => $this->request->getVar('jk'),
-            'kelas'             => $this->request->getVar('kelas'),
-            'nis'               => $this->request->getVar('nis'),
-            'nisn'              => $this->request->getVar('nisn'),
-            'nik'               => $this->request->getVar('nik'),
-            'no_kk'             => $this->request->getVar('kk'),
-            'anak_ke_berapa'    => $this->request->getVar('anak_ke_berapa'),
-            'tempat_lahir'      => $this->request->getVar('tempatlahir'),
-            'tanggal_lahir'     => $this->request->getVar('tgl_lahir'),
-            'no_akte'           => $this->request->getVar('no_akte'),
-            'agama'             => $this->request->getVar('agama'),
-            'alamat'            => $this->request->getVar('alamat'),
-            'rt'                => $this->request->getVar('rt'),
-            'rw'                => $this->request->getVar('rw'),
-            'dusun'             => $this->request->getVar('dusun'),
-            'desa'              => $this->request->getVar('desa'),
-            'kecamatan'         => $this->request->getVar('kecamatan'),
-            'pos'               => $this->request->getVar('pos'),
-            'tempat_tinggal'    => $this->request->getVar('tinggal'),
-            'transportasi'      => $this->request->getVar('transportasi'),
-            'created_at'        => date('Y-m-d')
-        ];
-
-        $sesi = [
-            'nis'   => $this->request->getVar('nis'),
-            'kelas' => $this->request->getVar('kelas'),
-        ];
-
-        session()->set($sesi);
-
-        $this->biodataModel->add($data);
-
-        return redirect()->to('/dataayah');
     }
 
     public function view()
